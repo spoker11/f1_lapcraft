8be4b9a5ad02aee4779e7d2b6d665503a3b78d1a
spoker11 <cs.go11@seznam.cz>
2025-09-01 09:26:20 +0200

    specifikace komentaru

--- f1viz/t_performance.py
+++ f1viz/t_performance.py
@@ -28,7 +28,7 @@ data = {
         "Haas F1 Team",
         "Alpine F1 Team"
     ],
-    "Body": [559, 260, 236, 194, 70, 51, 45, 44, 35, 20]
+    "Body": [584, 260, 248, 214, 80, 51, 60, 62, 44, 20]
 }
 
 def show_team_standings():
