41db4e422d5935f5650f352806f553e2b6b5a38c
spoker11 <cs.go11@seznam.cz>
2025-12-03 09:38:18 +0100

    zmena velke ceny

--- f1viz/kalendar_f1.py
+++ f1viz/kalendar_f1.py
@@ -3,10 +3,10 @@ import datetime
 
 def show_calendar():
     #Nastavení informací o dalším závodě
-    next_race_name = "Velká cena Brazílie 🇧🇷"
+    next_race_name = "Velká cena Abú Zabí 🇦🇪"
     next_race_title = "Začíná za:"
-    next_race_date = "2025-11-09"
-    next_race_time = "17:00"   # 24h format
+    next_race_date = "2025-12-07"
+    next_race_time = "16:00"   # 24h format
 
     #Sestavení datetime pro závod
     race_dt = datetime.datetime.strptime(f"{next_race_date} {next_race_time}", "%Y-%m-%d %H:%M")
